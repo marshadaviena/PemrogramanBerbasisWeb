6542eef5314b1fa7a142692d49cba22969be91c5
marshadaviena <165929567+marshadaviena@users.noreply.github.com>
2025-03-28 14:12:59 +0700

    Add files via upload

--- Praktikum/Pertemuan5/script.js
+++ Praktikum/Pertemuan5/script.js
@@ -1,20 +1,69 @@
-function cekNilai() {
-    let nilai = parseInt(document.getElementById("nilai").value);
-    let hasil = "";
-    
-    if (nilai >= 80 && nilai <= 100) {
-        hasil = "Huruf Mutu: A";
-    } else if (nilai >= 70 && nilai <= 79) {
-        hasil = "Huruf Mutu: B";
-    } else if (nilai >= 60 && nilai <= 69) {
-        hasil = "Huruf Mutu: C";
-    } else if (nilai >= 50 && nilai <= 59) {
-        hasil = "Huruf Mutu: D";
-    } else if (nilai >= 0 && nilai <= 49) {
-        hasil = "Huruf Mutu: E";
-    } else {
-        hasil = "Nilai tidak valid!";
-    }
-    
-    document.getElementById("hasil").innerText = hasil;
-}
+// alert('halo wasap gengs') // pop up
+// console.log('ini sebuah console') // inspect
+
+// function contohLet() {
+//     let y = 10;
+//     if (true) {
+//         let y = 20;
+//         console.log("Nilai dalam blok:", y);
+//     }
+//     console.log("Nilai luar blok:", y);
+// }
+
+// contohLet();
+
+// let nama = 'Nur Salamah Azzahrah'; // string
+// let usia = 20; // integer
+// let tinggiBadan = 148.8; // double atau float
+// let beratBadan;
+// beratBadan = 43.5; // double atau float
+// let uang = null;
+
+// if (uang == null) {
+//     uang = "belum ada";
+// } else {
+//     uang = "udah ada";
+// }
+
+// switch (uang) {
+//     case 1:
+//         uang = "punya sedikit";
+//         break;
+//     case 2:
+//         uang = "punya cukup";
+//         break;
+//     default:
+//         uang = "tidak ada";
+//         break;
+// }
+
+// let uangAwal = 20000;
+// let uangTambahan = 15000;
+// const uangTotal = uangAwal - uangTambahan; // aritmatika
+
+// alert('nama saya ' + nama) // pop up
+// alert(`nama saya ${nama}, usia saya ${usia} tahun, tinggi badan saya ${tinggiBadan} cm`);
+
+// alert(`uang awal gue sebesar Rp.${uangAwal} dan gue dikasih tambahan Rp.${uangTambahan}, jadi totalnya Rp.${uangTotal}`);
+
+// let namaPanggilan = [
+//     'Nur',  // index 0
+//     'Ara',  // index 1
+//     'Nuy'   // index 2
+// ];
+
+// let mahasiswa = {
+//     nama: "Rina",
+//     usia: 20,
+//     jurusan: "Informatika"
+// }; // Object
+
+// console.log(mahasiswa.jurusan);
+
+// namaPanggilan.push('Nun', 'Zahra'); // push: menambahkan isi array di akhir
+// namaPanggilan.shift(); // shift: menghapus salah satu index array dari awal
+// namaPanggilan.pop(); // pop: menghapus salah satu index array dari akhir
+
+// alert(namaPanggilan);
+
+// console.log(namaPanggilan.length); // menampilkan panjang array
